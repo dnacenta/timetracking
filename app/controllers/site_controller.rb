c198b093a4f1d279793853dd5de600ce56165b0e
Daniel <vader0@protonmail.com>
2016-11-14 11:12:59 +0100

    calculator1

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,8 +1,16 @@
 class SiteController < ApplicationController
   def home
-    render 'home'
   end
+
   def contact
-    render 'contact'
+  end
+
+  def calculator
+  end
+
+  def calculate
+    @num1 = params[:first_num].to_f.round
+    @num2 = params[:second_num].to_f.round
+    @result = @num1 + @num2
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
+  get '/calculator', to: 'site#calculator'
+  post '/calculate', to: 'site#calculate'
 end
