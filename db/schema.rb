9cd4323c68677cdca71e6088ffec885d297a8b03
Daniel <vader0@protonmail.com>
2016-11-15 10:18:14 +0100

    database2

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115085536) do
+ActiveRecord::Schema.define(version: 20161115091726) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "year"
     t.integer  "month"
+    t.text     "description"
   end
 
 end
