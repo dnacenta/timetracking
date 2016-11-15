3394964737abdc742014f8c9c772da25851f4708
Daniel <vader0@protonmail.com>
2016-11-15 12:42:23 +0100

    db4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
+
   get '/calculator', to: 'site#calculator'
   post '/calculate', to: 'site#calculate'
+
+  get '/project', to: 'projects#index'
 end
